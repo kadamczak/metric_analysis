a58e58fa04c1bef76d8e06c5a1f6ccc2b09b5a4c
kadamczak <cynder77@wp.pl>
2025-07-14 22:47:54 +0200

    Trim bibtex dataset

--- src/experiment/helpers/variables.py
+++ src/experiment/helpers/variables.py
@@ -6,19 +6,6 @@ from pathlib import Path
 
 dataset_root_dir = Path('c:/VisualStudioRepositories/MUSIC_DATA/datasets')
 
-dataset_scene_dir = dataset_root_dir / 'multilabel' / 'scene'
-dataset_scene_trimmed_dir = dataset_root_dir / 'multilabel' / 'scene_trimmed'
-
-dataset_emotions_dir = dataset_root_dir / 'multilabel' / 'emotions'
-dataset_emotions_trimmed_dir = dataset_root_dir / 'multilabel' / 'emotions_trimmed'
-
-dataset_yeast_dir = dataset_root_dir / 'multilabel' / 'yeast'
-
-dataset_cal500_dir = dataset_root_dir / 'multilabel' / 'cal500'
-
-dataset_enron_dir = dataset_root_dir / 'multilabel' / 'enron'
-
-
 # =================
 # Output
 # =================
