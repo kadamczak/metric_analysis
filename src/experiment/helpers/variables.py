7e95ce01a8685f9eca670fd180ace3c9762df98b
kadamczak <cynder77@wp.pl>
2025-07-14 12:41:54 +0200

    Trim the emotions dataset

--- src/experiment/helpers/variables.py
+++ src/experiment/helpers/variables.py
@@ -4,16 +4,19 @@ from pathlib import Path
 # Datasets
 # =================
 
-dataset_root_dir = Path('c:\VisualStudioRepositories\MUSIC_DATA\datasets')
+dataset_root_dir = Path('c:/VisualStudioRepositories/MUSIC_DATA/datasets')
 
 dataset_scene_dir = dataset_root_dir / 'multilabel' / 'scene'
 dataset_scene_trimmed_dir = dataset_root_dir / 'multilabel' / 'scene_trimmed'
+
+dataset_emotions_dir = dataset_root_dir / 'multilabel' / 'emotions'
+dataset_emotions_trimmed_dir = dataset_root_dir / 'multilabel' / 'emotions_trimmed'
 
 
 # =================
 # Output
 # =================
-report_output_root_dir = Path('C:\VisualStudioRepositories\MUSIC_DATA\metric_analysis\output')
+report_output_root_dir = Path('C:/VisualStudioRepositories/MUSIC_DATA/metric_analysis/output')
 
 report_output_dirs = {
     name: report_output_root_dir / name
