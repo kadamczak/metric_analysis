d60e3f00181cc19f5885bd5ba1bc20f1bd0fed4a
kadamczak <cynder77@wp.pl>
2025-07-14 14:31:54 +0200

    Process the yeast dataset

--- src/experiment/helpers/variables.py
+++ src/experiment/helpers/variables.py
@@ -12,17 +12,14 @@ dataset_scene_trimmed_dir = dataset_root_dir / 'multilabel' / 'scene_trimmed'
 dataset_emotions_dir = dataset_root_dir / 'multilabel' / 'emotions'
 dataset_emotions_trimmed_dir = dataset_root_dir / 'multilabel' / 'emotions_trimmed'
 
+dataset_yeast_dir = dataset_root_dir / 'multilabel' / 'yeast'
+
+dataset_cal500_dir = dataset_root_dir / 'multilabel' / 'cal500'
+
+dataset_enron_dir = dataset_root_dir / 'multilabel' / 'enron'
+
 
 # =================
 # Output
 # =================
 report_output_root_dir = Path('C:/VisualStudioRepositories/MUSIC_DATA/metric_analysis/output')
-
-report_output_dirs = {
-    name: report_output_root_dir / name
-    for name in [
-        'binary_balanced', 'binary_unbalanced',
-        'multiclass_balanced', 'multiclass_unbalanced',
-        'multilabel_balanced', 'multilabel_unbalanced'
-    ]
-}
