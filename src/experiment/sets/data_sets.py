93052183371d19cc665b89a117afb6c50e8a006a
kadamczak <cynder77@wp.pl>
2025-07-18 19:29:37 +0200

    Analyze and trim stackexcs dataset

--- src/experiment/sets/data_sets.py
+++ src/experiment/sets/data_sets.py
@@ -1,24 +1,24 @@
 from src.experiment.helpers.variables import dataset_root_dir
 
 multilabel_datasets = {
-    "bibtex_trimmed": {
-        "path": dataset_root_dir / "multilabel" / "bibtex_trimmed",
-        "classification_type": "multilabel",
-        "class_balance": "balanced",
-        "dataset_name": "bibtex_trimmed",
-    },
+    # "bibtex_trimmed": {
+    #     "path": dataset_root_dir / "multilabel" / "bibtex_trimmed",
+    #     "classification_type": "multilabel",
+    #     "class_balance": "balanced",
+    #     "dataset_name": "bibtex_trimmed",
+    # },
     # "bookmarks_trimmed": {
     #     "path": dataset_root_dir / "multilabel" / "bookmarks_trimmed",
     #     "classification_type": "multilabel",
     #     "class_balance": "balanced",
     #     "dataset_name": "bookmarks_trimmed",
     #},
-     "emotions_trimmed": {
-         "path": dataset_root_dir / "multilabel" / "emotions_trimmed",
-         "classification_type": "multilabel",
-         "class_balance": "balanced",
-         "dataset_name": "emotions_trimmed",
-     },
+    #  "emotions_trimmed": {
+    #      "path": dataset_root_dir / "multilabel" / "emotions_trimmed",
+    #      "classification_type": "multilabel",
+    #      "class_balance": "balanced",
+    #      "dataset_name": "emotions_trimmed",
+    #  },
     # "scene_trimmed": {
     #     "path": dataset_root_dir / "multilabel" / "scene_trimmed",
     #     "classification_type": "multilabel",
