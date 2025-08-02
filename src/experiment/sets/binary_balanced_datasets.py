937c924a5f222d216b1475d06ee69f1402e8594c
kadamczak <cynder77@wp.pl>
2025-08-02 19:05:48 +0200

    Add ceas dataset

--- src/experiment/sets/binary_balanced_datasets.py
+++ src/experiment/sets/binary_balanced_datasets.py
@@ -1,8 +1,9 @@
 from src.experiment.helpers.variables import dataset_root_dir
 
 dataset_names = [
-    "all-in-one_sentiment_balanced",
+    #"all-in-one_sentiment_balanced",
     #"amazon-reviews-0.25_balanced",
+    "ceas_balanced",
     #"colon-0.5_balanced",
     #"fake-news_balanced",
     #"news-sarcasm_balanced",
--- src/experiment/sets/binary_imbalanced_datasets.py
+++ src/experiment/sets/binary_imbalanced_datasets.py
@@ -1,8 +1,9 @@
 from src.experiment.helpers.variables import dataset_root_dir
 
 dataset_names = [
-    "all-in-one_sentiment_imbalanced",
+    #"all-in-one_sentiment_imbalanced",
     #"amazon-reviews-0.25_imbalanced",
+    "ceas_imbalanced",
     #"colon-0.5_imbalanced",
     #"fake-news_imbalanced",
     #"news-sarcasm_imbalanced",
